bb028b0a8ef7f8ab7b153d3288fc23e7b83ada5a
SHUVO <im.computer.engineeer@gamil.com>
2025-08-31 14:14:42 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Navbar from "./assets/Components/Navbar";
+
+function App() {
+  return (
+    <div className="relative overflow-x-hidden text-stone-300 antialiased min-h-screen">
+      {/* Background */}
+      <div className="fixed inset-0 -z-10">
+        <div className="absolute inset-0 h-full w-full [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
+      </div>
+
+      {/* Main Content */}
+      <div className="container mx-auto px-8">
+        <Navbar />
+        {/* Your other content here */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
