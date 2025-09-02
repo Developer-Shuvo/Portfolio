1ba5ca71228d8693a4a64f848d2af03684f1fe10
SHUVO <im.computer.engineeer@gamil.com>
2025-09-02 15:49:10 +0600

    Projects section animation added

--- src/assets/Components/Projects/Projects.jsx
+++ src/assets/Components/Projects/Projects.jsx
@@ -1,4 +1,3 @@
-
 import { PROJECTS } from "../../../../constants";
 // Removed 'Technologies' as it was incorrectly imported and used
 // You should be using 'tech' from the map function
@@ -8,10 +7,13 @@ const Projects = () => {
   return (
     <div className="p-4 ">
       <motion.h1
-      whileInView={{opacity:1, y:0}}
-      initial= {{opacity:0, y: -100}}
-      transition= {{duration: 0.5}}
-      className="my-20 text-center text-4xl">Projects</motion.h1>
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 0.5 }}
+        className="my-20 text-center text-4xl"
+      >
+        Projects
+      </motion.h1>
 
       <div className="">
         {PROJECTS.map(
@@ -24,7 +26,12 @@ const Projects = () => {
               className="mb-8 border border-[#2f176f] border-0.5 p-4 flex flex-wrap lg:justify-center"
             >
               {/* Project Image */}
-              <div className="w-full lg:w-1/4">
+              <motion.div
+                whileInView={{ opacity: 1, x: 0 }}
+                initial={{ opacity: 0, x: -100 }}
+                transition={{ duration: 1 }}
+                className="w-full lg:w-1/4"
+              >
                 <img
                   src={project.image} // Changed from Projects.image
                   width={250}
@@ -32,10 +39,15 @@ const Projects = () => {
                   alt={project.title} // Corrected alt text to be dynamic
                   className="mb-6 rounded lg:mr-4" // Added margin-right for spacing
                 />
-              </div>
+              </motion.div>
 
               {/* Project Details (Title, Description, and Technologies) */}
-              <div className="w-full max-w-xl lg:w-3/4">
+              <motion.div
+                whileInView={{ opacity: 1, x: 0 }}
+                initial={{ opacity: 0, x: 100 }}
+                transition={{ duration: 1 }}
+                className="w-full max-w-xl lg:w-3/4"
+              >
                 <h3 className="mb-2 font-semibold text-2xl">
                   {project.title} {/* Changed from Projects.title */}
                 </h3>
@@ -55,7 +67,7 @@ const Projects = () => {
                     </span>
                   ))}
                 </div>
-              </div>
+              </motion.div>
             </div>
           )
         )}
